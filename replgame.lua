710541844eb6395c9db7b900467f5acfb9361bb4
Kamefrede <kamefrede@gmail.com>
2016-09-03 22:19:24 +0100

    Added the final story. TBD: Fight and tie in to the initial story

--- replgame.lua
+++ replgame.lua
@@ -71,9 +71,9 @@ end
 
 function sleepy()
 	io.write("You suddenly wake up for no apparent reason and find yourself binded by chains in an even smaller room\n")
-	sleep(3)
+	sleep(5)
 	io.write("After roughly 2 days of just being there, you start getting dizzy because you haven't drank any water in two days\n")
-	sleep(3)
+	sleep(5)
 	io.write("2 days later you die from dehydration.\n")
 	gameOver()
 end
@@ -91,6 +91,48 @@ function gameOver()
 		return false
 	end
 
+end
+
+function treasure_room()
+	io.write("You manage to open the huge door.\n On the other side of the door there is a treasure room! Yes! \n As you walk something catches your eye. \n !!!! Seems to be a treasure chest! Talk about luck! \n Spoke too soon. Monster incoming at 10 o' clock. Now that's some luck alright. \n It's an ogre! He's armored to the teeth and seems to be carrying a cub.\n Do you want to fight it?")
+	local response = io.read()
+	if check(yes, response) == true then
+		io.write("You pull out your " .. player weapon .. " to fight the ogre.")
+		fight()
+	else
+		io.write("You don't pull out your weapon, but then realize it was a bad idea because there is no other way out.")
+		sleep(5)
+		io.write("You get killed by the ogre.")
+		gameOver()
+end
+
+function fight()
+	--tbd
+end
+
+function previctory()
+	io.write("You're finally able to approach the chest!\n Hm.. the lid is a bit heavy. It must be a good prize inside.\n With the suspense killing you, you have to decide either to open it or not.\n Open it? Yes/No \n")
+	local response = io.read()
+	if check(yes, response) == true then
+		victory()
+	else
+		io.write("You spend a long time contemplating if you should open it or not, you spend just enough time for the ogre's girlfriend to sneak up behind you and kill you!\n Congratulations you died because you didn't want a prize.\n"
+		gameOver()
+	end
+end
+
+function victory()
+	io.write("You pick up the diamond from the treasure chest and as you raise it up with your hand it reflects a light beam and opens a passageway!\n As it is the only sane move to do, you run through the passageway and magically get teleported to the entrace of your house.\n Victory! Hurray! \n Play Again Yes/No \n")
+	local response = io.read()
+	if check(yes, response) == true then
+		for i=1,30 do
+			io.write("\n")
+			chooseName()
+		end
+	else
+		io.write("Thanks for playing \n")
+		return false
+	end
 end
 
 function check(table, checkie)
