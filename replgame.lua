4c0de3d5d9291bf296f7816c58b6f1e53ea5ad0d
Kamefrede <kamefrede@gmail.com>
2016-09-03 21:36:04 +0100

    Added initial gameplay and story

--- replgame.lua
+++ replgame.lua
@@ -3,6 +3,7 @@ player_health = 100
 monster_health = 100
 yes = { "Yes", "Y", "Yeah", "Yas", "Yeh" }
 no =  {"No", "N", "Nah", "Nope", "lolno" }
+clock = os.clock
 
 function chooseName()
 	io.write("Hello Young Traveler, what is thy name?\n")
@@ -15,7 +16,7 @@ function chooseWeapon()
 	io.write("Would you like a Dagger, a Crossbow, a Gun, a Sickle or a Wand?\n")
 	player_weapon = io.read()
 	if check(weapons, player_weapon) == true then
-		startGame()
+		confirm()
 	else 
 		print("That's not quite right would you like to choose again?")
 		local response = io.read()
@@ -28,8 +29,68 @@ function chooseWeapon()
 	end
 end
 
+function confirm()
+	io.write("The old man asks: \n"
+	io.write("Are you brave enough to enter the dungeon? Yes/No \n")
+	local response = io.read()
+	if check(yes, response) == true then
+		startGame()
+	else
+		io.write("You cowardly ran back to the old man, returning your " .. player_weapon .. " to him and exiting the dungeon.\n")
+		io.write("Play Again? Yes/No \n")
+		local response2 = io.read()
+		if check(yes, response) == true then
+			chooseName()
+		else
+			print("Thanks for playing!\n")
+			return false
+		end
+	end
+end
+
 function startGame()
-	print("hey atleast you got it to work")
+	io.write("After saying yes to the old man you black out\n")
+	io.write("...")
+	sleep(0.5)
+	io.write("...")
+	sleep(0.5)
+    initialroom()
+end
+
+function initialroom()
+	io.write("You wake up in a small locked room. In front of you, you see a large door that appears to be locked.\n")
+	io.write("Get up and look around? Yes/No \n")
+	local response = io.read()
+	if check(yes, response) == true then
+		io.write("stuff")
+	else
+		io.write("You decide not to do anything and you eventually fall asleep. \n")
+		sleepy()
+	end
+end
+
+function sleepy()
+	io.write("You suddenly wake up for no apparent reason and find yourself binded by chains in an even smaller room\n")
+	sleep(3)
+	io.write("After roughly 2 days of just being there, you start getting dizzy because you haven't drank any water in two days\n")
+	sleep(3)
+	io.write("2 days later you die from dehydration.\n")
+	gameOver()
+end
+
+function gameOver()
+	io.write("Game Over, Play Again? Yes/No")
+	local response = io.read()
+	if check(yes, response) == true then
+		for i=1,30 do
+			io.write("\n")
+			chooseName()
+		end
+	else
+		io.write("Thanks for playing \n")
+		return false
+	end
+
 end
 
 function check(table, checkie)
@@ -40,5 +101,11 @@ function check(table, checkie)
 	end
 return false
 end
+
+function sleep(n)
+	local t0 = clock()
+	while clock() - t0 <= n do end
+end
+
 
 chooseName()
