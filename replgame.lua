f938c00ce7b9855e3693c26b5aec08ed311191ac
Kamefrede <kamefrede@gmail.com>
2016-09-03 21:11:29 +0100

    Rewrote the name and weapon selection code

--- replgame.lua
+++ replgame.lua
@@ -1,40 +1,44 @@
-
+weapons =  { "Dagger", "Crossbow", "Gun", "Sickle", "Wand" } 
 player_health = 100
 monster_health = 100
-function introduction()
-io.write("Hello Young Traveler, what is thy name?\n")
-player_name = io.read()
-io.write("Very well " .. player_name .. ", what weapon would you want?\n")
-io.write("Would you like a Dagger, a Crossbow, a Gun, a Sickle or a Wand?\n")
-player_weapon = io.read()
-if player_weapon == "Dagger" then
-	io.write("Okay, " .. player_name .. " you chose to wield " .. player_weapon ..  " as your trusty weapon!\n")
-elseif player_weapon == "Crossbow" then
-	io.write("Okay, " .. player_name .. " you chose to wield " .. player_weapon ..  " as your trusty weapon!\n")
-elseif player_weapon == "Crossbow" then
-	io.write("Okay, " .. player_name .. " you chose to wield " .. player_weapon ..  " as your trusty weapon!\n")
-elseif player_weapon == "Gun" then
-	io.write("Okay, " .. player_name .. " you chose to wield " .. player_weapon ..  " as your trusty weapon!\n")
-elseif player_weapon == "Sickle" then
-	io.write("Okay, " .. player_name .. " you chose to wield " .. player_weapon ..  " as your trusty weapon!\n")
-elseif player_weapon == "Wand" then
-	io.write("Okay, " .. player_name .. " you chose to wield " .. player_weapon ..  " as your trusty weapon!\n")
-else
-	io.write("That's not quite right!\n")
-end
+yes = { "Yes", "Y", "Yeah", "Yas", "Yeh" }
+no =  {"No", "N", "Nah", "Nope", "lolno" }
+
+function chooseName()
+	io.write("Hello Young Traveler, what is thy name?\n")
+	player_name = io.read()
+	chooseWeapon()
 end
 
-function start_game()
-io.write("Are you ready to play? Yes/No")
-response = io.read()
-if response == "Yes" then
-	enter_room
-elseif response == "Y" then
-	enter_room
-else
-	io.write("Game Over!")
-	introduction()
-end
+function chooseWeapon()
+	io.write("Very well " .. player_name .. ", what weapon would you want?\n")
+	io.write("Would you like a Dagger, a Crossbow, a Gun, a Sickle or a Wand?\n")
+	player_weapon = io.read()
+	if check(weapons, player_weapon) == true then
+		startGame()
+	else 
+		print("That's not quite right would you like to choose again?")
+		local response = io.read()
+		if check(yes, response) == true then
+			chooseWeapon()
+		else
+			print("Thanks for playing!\n")
+			return false
+		end
+	end
 end
 
-introduction()
+function startGame()
+	print("hey atleast you got it to work")
+end
+
+function check(table, checkie)
+	for key,value in pairs(table) do
+		if value == checkie then
+			return true
+		end
+	end
+return false
+end
+
+chooseName()
